ef9b4d6968808b9556a43f8ebf2bbbb075478b60
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-04-21 09:16:07 +0100

    Debugged the errors and displayed us

---
+++ capstone-thirdweb-go-sip-read/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>About This App</h1>
+        <p>Hey 👋 I am PAFEKZY, and this is my Thirdweb + Next.js Capstone!</p>
+      </div>
+    );
+  }
