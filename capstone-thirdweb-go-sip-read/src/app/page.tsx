8df4d71841bbe5ea8cfc5a1750a68314258a201a
github.com/Pafekzy <github.com/Pafekzy.gmail.com>
2025-04-21 15:23:28 +0100

    Added Use the <ClaimBadge /> tag where you want the button to appear in your layout

--- capstone-thirdweb-go-sip-read/src/app/page.tsx
+++ capstone-thirdweb-go-sip-read/src/app/page.tsx
@@ -1,3 +1,4 @@
+import ClaimBadge from "@/components/Claimbadge";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        {/* Add the ClaimBadge component here */}
+        <ClaimBadge />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
